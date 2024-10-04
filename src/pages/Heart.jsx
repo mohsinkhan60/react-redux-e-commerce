878450488e8b614edb7cc373daefc1ea8e7ca5a8
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-10-05 00:15:48 +0500

    add HeartTotalPrice Section

--- src/pages/Heart.jsx
+++ src/pages/Heart.jsx
@@ -1,10 +1,12 @@
 import HeartHead from "../components/Heart/HeartHead"
+import HeartTotalPrice from "../components/Heart/HeartTotalPrice"
 
 
 export const Heart = () => {
   return (
     <div>
       <HeartHead />
+      <HeartTotalPrice />
     </div>
   )
 }
